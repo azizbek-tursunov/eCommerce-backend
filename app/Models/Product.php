33cd9c1acc20833fc6756509c65dd5dbc25d1370
azizbek-tursunov <azizbektursunovofficial@gmail.com>
2023-08-15 13:34:29 +0500

    Product resource

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
 
     protected $fillable = ['name', 'price', 'description', 'category_id'];
+
+    public array $translatable = ["name", "description"];
 
     public function category()
     {
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -30,6 +30,32 @@ class ProductSeeder extends Seeder
                     ],
                 ])
             ]);
+            $product->stocks()->create([
+                "quantity" => rand(1, 10),
+                "attributes" => json_encode([
+                    [
+                        "attribute_id" => 1,
+                        "value_id" => rand(1, 4)
+                    ],
+                    [
+                        "attribute_id" => 2,
+                        "value_id" => rand(5, 8)
+                    ],
+                ])
+            ]);
+            $product->stocks()->create([
+                "quantity" => rand(1, 10),
+                "attributes" => json_encode([
+                    [
+                        "attribute_id" => 1,
+                        "value_id" => rand(1, 4)
+                    ],
+                    [
+                        "attribute_id" => 2,
+                        "value_id" => rand(5, 8)
+                    ],
+                ])
+            ]);
         }
     }
 }
